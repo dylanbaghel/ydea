ac21701228adf71ff0d019979496f2bf51bc326f
dylanbaghel <baghelabhi39@gmail.com>
2018-08-24 15:28:57 +0530

    fixed config file

--- config/config.js
+++ config/config.js
@@ -4,4 +4,7 @@ console.log('env************', env);
 if (env === 'development') {
     process.env.PORT = 4400;
     process.env.MONGO = 'mongodb://localhost:27017/VidjotApp';
+} else if (env === 'production') {
+    process.env.PORT = 4400;
+    process.env.MONGO = 'mongodb://dylan:anya2692@ds131942.mlab.com:31942/vidjot-app';
 }
