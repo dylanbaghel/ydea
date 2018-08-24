eb8656f20723aba17dc4ffaac06341c34b1881fd
dylanbaghel <baghelabhi39@gmail.com>
2018-08-24 15:33:17 +0530

    fixed wrong config

--- config/config.js
+++ config/config.js
@@ -5,6 +5,5 @@ if (env === 'development') {
     process.env.PORT = 4400;
     process.env.MONGO = 'mongodb://localhost:27017/VidjotApp';
 } else if (env === 'production') {
-    process.env.PORT = 4400;
     process.env.MONGO = 'mongodb://dylan:anya2692@ds131942.mlab.com:31942/vidjot-app';
 }
